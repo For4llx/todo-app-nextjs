c6aa621ecd3dc3102f51f6479cfb25aeb4be834d
Nobime Romeo <nobimeromeo@pop-os.localdomain>
2023-07-07 16:45:01 +0200

    modified

--- src/app/active/page.tsx
+++ src/app/active/page.tsx
@@ -8,7 +8,9 @@ export default function Todos() {
   const { todos, setTodos } = useContext(TodoContext);
 
   useEffect(() => {
-    fetch(`./api/todo/active`, { cache: "no-store" })
+    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/todo/active`, {
+      cache: "no-store",
+    })
       .then((response) => response.json())
       .then((data) => setTodos(data.todos));
   }, []);
